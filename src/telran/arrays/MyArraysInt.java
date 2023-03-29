39c3bd3ecc485753edae9ade511212b4ce277c3b
DimaOstanin <xnadab@gmail.com>
2023-03-29 18:57:39 +0300

    HW2 after checking

--- src/telran/arrays/MyArraysInt.java
+++ src/telran/arrays/MyArraysInt.java
@@ -19,14 +19,14 @@ public class MyArraysInt {
     	   //Returns the source array if for a wrong index value (less than 0 or greater than array.length -1 )
     	    //using standard method System.arraycopy
 //    	   int s[] = {5,10,3,'2',20};
-    	  
+    	   int []newArr = array;
     	  if (index >= 0 && index <= array.length-1) {
-    	  int []newArr = new int[array.length-1];
+    	  newArr = new int[array.length-1];
     	  System.arraycopy(array, 0, newArr, 0, index);
-  		  System.arraycopy(array, index+1, newArr, index, (array.length-1)-index);
-  		    array = newArr;
+  		  System.arraycopy(array, index+1, newArr, index, newArr.length-index);
+    
     	  }
-    	   return array;
+    	   return newArr;
        }
        
        public static int[] insertNumber(int[]array,int index, int num) {
@@ -34,16 +34,15 @@ public class MyArraysInt {
     	   //Insert a given num at a given index for right value of the index [0 - array.length] 
     	   //and returns new array with the inserted number
     	   //return the source array for a wrong index value (less than 0 or greater than array.length)
-    	   //using standard method System.arraycopy
-    	   
+    	   //using standard method System.arraycopy    	   
 //    	   int s[] = {1,2,3,4,5,6,7,8};
+    	  int []newArr = array;
     	  if (index >= 0 && index <= array.length) {
-    	  int []newArr = new int[array.length+1];
+    	  newArr = new int[array.length+1];
     	  System.arraycopy(array, 0, newArr, 0, index);
-  		  System.arraycopy(array, index, newArr, index+1, array.length - index);
-  		  newArr[index]= num;
-  		   array = newArr;
+  		  System.arraycopy(array, index, newArr, index+1, newArr.length-1 - index);
+  		  newArr[index]= num;  
     	  }
-    	   return array;
+    	   return newArr;
        }
 }
